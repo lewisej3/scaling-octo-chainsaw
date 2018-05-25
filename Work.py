3a7cd5149df3d4a8208b2d1d98c5058a02849a83
lewisej3 <39620993+lewisej3@users.noreply.github.com>
2018-05-25 13:22:37 +0100

    Add files via upload
    testing this

---
+++ Work.py
@@ -0,0 +1,8 @@
+def getAuthorName():
+    return "Edward Lewis"
+
+text = '\'This is a quote \
+that goes onto two lines'
+text= text.upper()
+print(text)
+print("Author Name is : " + getAuthorName())
