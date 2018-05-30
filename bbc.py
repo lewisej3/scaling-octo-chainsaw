0433ef54c9a17d129ace8f8c62101d1a8198dff9
lewisej3 <39620993+lewisej3@users.noreply.github.com>
2018-05-30 20:44:55 +0100

    Update bbc.py

--- bbc.py
+++ bbc.py
@@ -1,49 +1,78 @@
 #!/usr/local/bin/python3
 
+import datetime
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
-
 response = requests.get('http://www.bbc.co.uk/news')
 soup = BeautifulSoup(response.text, 'html.parser')
 
-#Scrape the primary headline with summary and link
+now = datetime.datetime.now()
+currentdatetime = now.strftime("%Y-%m-%d %H:%M")
+sqlite_file = 'bbcnews.sqlite'    # name of the sqlite database file
+
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+
+def add_item_sum(category):
+    c.execute('''INSERT INTO News(Headlines, Summary, Link, Date, Type) \
+        VALUES (?, ?, ?, ?, ?)''', [headline.text, summary.text, link['href'], currentdatetime, category])
+    return 0
+def add_item_nosum(category):
+    c.execute('''INSERT INTO News(Headlines, Summary, Link, Date, Type) \
+        VALUES (?, ?, ?, ?, ?)''', [headline.text, " ", link['href'], currentdatetime, category])
+    return 0
+
+# Creating a new SQLite table
+try:
+    c.execute('''CREATE TABLE News
+              (Headlines TEXT,
+               Summary TEXT,
+               Link TEXT,
+               Date TEXT,
+        Type TEXT)''');
+except:
+    pass
+
+# Find the primary headline and input as new row in table.
+
 primary = soup.find('div',{'class': 'nw-c-top-stories-primary__story'} )
 headline =primary.find('h3')
 summary = primary.find('p')
 link = primary.find('a')
-print('Primary items')
-print(headline.text)
-print(summary.text)
-print(link['href'])
+add_item_sum("Primary")
 
-#Scrape the secondary headlines with summaries (if applicable) and links
+# Find the secondary headlines and input as new rows in tables.
+# If the secondary headline doesn't have a summary then it adds a blank space
 secondaries = soup.find_all('div',{'class': 'nw-c-top-stories__secondary-item'} )
-print('Secondary items')
 for secondary in secondaries:
     headline = secondary.find('h3')
     summary = secondary.find('p')
     link = secondary.find('a')
-    print(headline.text)
     if summary:
-        print(summary.text)
-    print(link['href'])
+        add_item_sum("Secondary")
+    else:
+        add_item_nosum("Secondary")
 
-#Scrape the tertiary headlines with links
+# Find the tertiary headlines and input as new rows in tables
 tertiary = soup.find('div',{'class': 'nw-c-top-stories__tertiary-items'})
-print('Tertiary items')
 stories = tertiary.find_all('div',{'class': 'gel-layout__item'})
 for story in stories:
     headline = story.find('h3')
-    print(headline.text)
     link = story.find('a')
-    print(link['href'])
+    add_item_nosum("Tertiary")
 
-#Scrape the Sport's headlines with links
+# Find the Sport's headlines with links
+
 sport = soup.find('div',{'class': 'nw-c-sport'})
-print('Sport items')
 stories = sport.find_all('div', { 'class': 'gs-c-promo' })
 for story in stories:
     headline = story.find('h3')
-    print(headline.text)
     link = story.find('a')
-    print(link['href'])
+    add_item_nosum("Sport")
+
+
+# Committing changes and closing the connection to the database file
+conn.commit()
+conn.close()
